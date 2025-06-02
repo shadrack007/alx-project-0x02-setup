e8c488840b2cd5e49cb554fcdc3eeda812c0483f
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 17:35:28 +0300

    defined ActiveLinkProps

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -7,3 +7,10 @@ export interface ButtonProps {
   size: "small" | "medium" | "large";
   shape: "rounded-sm" | "rounded-md" | "rounded-full";
 }
+
+export interface ACtiveLinkProps {
+  href: string;
+  children: React.ReactNode;
+  activeClassName?: string;
+  className?: string;
+}
