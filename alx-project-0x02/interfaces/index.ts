0e22b7ffff189a5073cfca1436adf59ad86c7ba4
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 17:00:27 +0300

    feat: defined button props

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -2,3 +2,8 @@ export interface CardProps {
   title: string;
   content: string;
 }
+
+export interface ButtonProps {
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+}
