dd3b365ef9aa8edca288a1ab1f3f18a534bb1101
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 17:33:22 +0300

    feat: post page

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+const Posts: React.FC = () => {
+  return (
+    <section>
+      <Header />
+      <p>post page</p>
+    </section>
+  );
+};
+
+export default Posts;
