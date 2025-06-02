0da753d62d87a9b789d74c8d3612dfd943c4a521
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 15:53:13 +0300

    added 3 card

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,17 @@
+import Card from "@/components/common/Card";
+
 const Home: React.FC = () => {
   return (
     <section>
       <h1>Home page</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+        <Card title="card 1" content="Card 1 content goes here" />
+
+        <Card title="card 2" content="Card 3 content goes here" />
+
+        <Card title="card 3" content="Card 3 content goes here" />
+      </div>
     </section>
   );
 };
