3df1c5f184f641b82dcccd529b8ed7b27a298c36
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 14:58:52 +0300

    feat: implemented simple nav bar

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 const Header: React.FC = () => {
   return (
     <header>
-      <p>header</p>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
