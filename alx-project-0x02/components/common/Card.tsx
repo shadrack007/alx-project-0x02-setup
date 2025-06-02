b0cd784d7edb9640e320a3ebc18286361d70a264
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 15:08:22 +0300

    feat: implemented card

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,12 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default Card;
