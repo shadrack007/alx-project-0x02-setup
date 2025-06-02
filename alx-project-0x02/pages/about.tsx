ee9e7aefafa0eda30bdeac2b178d880d3b5a90be
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 17:11:43 +0300

    display 3 different button size

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,18 @@
+import Button from "@/components/common/Button";
+
 const About: React.FC = () => {
   return (
     <section>
       <h1>About page</h1>
+
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1>small button</h1>
+        <Button size="small" shape="rounded-sm" />
+        <h1>Medium Button</h1>
+        <Button size="medium" shape="rounded-md" />
+        <h3>Large Button</h3>
+        <Button size="large" shape="rounded-full" />
+      </div>
     </section>
   );
 };
