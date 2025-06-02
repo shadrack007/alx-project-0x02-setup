b86a16b93210145e74ecafa344c0f2afa0cb1e50
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 14:58:33 +0300

    added header

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/layout/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
+      <Header />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Welcome to airbnb clone project</h1>
       </main>
