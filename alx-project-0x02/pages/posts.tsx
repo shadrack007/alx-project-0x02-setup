ca806c37aae0f745989199abb96e1a6285f49e77
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-02 18:27:50 +0300

    fets posts

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,12 +1,42 @@
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
 
-const Posts: React.FC = () => {
+const Posts: React.FC = ({ posts }) => {
   return (
     <section>
       <Header />
-      <p>post page</p>
+
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+          {posts.map(
+            (
+              postItem: { title: string; body: string; userId: number },
+              index: number
+            ) => (
+              <PostCard
+                key={index}
+                title={postItem.title}
+                content={postItem.body}
+                userId={postItem.userId}
+              />
+            )
+          )}
+        </div>
+      </div>
     </section>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Posts;
